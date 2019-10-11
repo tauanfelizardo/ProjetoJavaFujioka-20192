a2026d31594d3d5035474891c1756deba8cf0b26
Wendell Clive Santos de Lira <wendell.clive@gmail.com>
2019-10-11 13:29:12 -0300

    exercicio fujioka

---
+++ src/main/java/br/com/generico/Exercicio10QueriesApplication.java
@@ -0,0 +1,13 @@
+package br.com.generico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicio10QueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercicio10QueriesApplication.class, args);
+	}
+
+}
